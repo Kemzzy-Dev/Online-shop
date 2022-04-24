9752b147a4f0a0233d0cc2d76464118d0f75a20e
kemzzy-dev <kemzzy17@gmail.com>
2022-04-24 03:03:00 +0100

    Finally got the home page to display, next is to make other pages display too.

--- shoponline/settings.py
+++ shoponline/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
